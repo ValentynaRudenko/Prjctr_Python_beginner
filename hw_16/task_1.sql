b9667f07046fc9e4e612d344b8c7bdf72e4e4a1b
Valentyna <valentino4ka2009@gmail.com>
2023-10-04 10:06:58 +0300

    sql corrected 1.0

--- hw_16/task_1.sql
+++ hw_16/task_1.sql
@@ -3,24 +3,14 @@ CREATE TABLE abn_user (
   username VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL,
   phone VARCHAR(20) NOT NULL,
-  registered_at TIMESTAMP
+  registered_at TIMESTAMP,
+  user_type VARCHAR(5) NOT NULL
 );
 
-CREATE TABLE host (
-  id serial PRIMARY KEY,
-  abn_user_id int NOT NULL,
-  FOREIGN KEY (abn_user_id) REFERENCES abn_user (id)
-);
-
-CREATE TABLE guest (
-  id serial PRIMARY KEY,
-  abn_user_id int NOT NULL,
-  FOREIGN KEY (abn_user_id) REFERENCES abn_user (id)
-);
 
 CREATE TABLE room (
   id serial PRIMARY KEY,
-  host_id int NOT NULL,
+  abn_user_id int NOT NULL,
   price_night float NOT NULL,
   amount_of_residents int NOT NULL,
   n_of_rooms int NOT NULL,
@@ -28,7 +18,7 @@ CREATE TABLE room (
   air_conditioning bool NOT NULL,
   refrigerator bool NOT NULL,
   kitchen bool NOT NULL,
-  FOREIGN KEY (host_id) REFERENCES host (id)
+  FOREIGN KEY (abn_user_id) REFERENCES abn_user (id)
 );
 
 CREATE TABLE reservation (
@@ -37,59 +27,47 @@ CREATE TABLE reservation (
   check_in date NOT NULL,
   check_out date NOT NULL,
   room_id int NOT NULL,
-  guest_id int,
+  abn_user_id int,
   FOREIGN KEY (room_id) REFERENCES room (id),
-  FOREIGN KEY (guest_id) REFERENCES guest (id)
-);
-
-CREATE TABLE availability (
-  room_id int NOT NULL,
-  checking_date date NOT NULL,
-  available bool NOT NULL,
-  FOREIGN KEY (room_id) REFERENCES room (id)
+  FOREIGN KEY (abn_user_id) REFERENCES abn_user (id)
 );
 
 CREATE TABLE payment (
   id serial PRIMARY KEY,
   paid_at timestamp NOT NULL,
-  room_id int NOT NULL,
-  guest_id int NOT NULL,
+  reservation_id int NOT NULL,
   payment_method varchar(50) NOT NULL,
-  FOREIGN KEY (room_id) REFERENCES room (id),
-  FOREIGN KEY (guest_id) REFERENCES guest (id)
+  amount decimal(7,2) NOT NULL,
+  FOREIGN KEY (reservation_id) REFERENCES reservation (id)
 );
 
 CREATE TABLE review (
   id serial PRIMARY KEY,
   review_date timestamp NOT NULL,
-  room_id int NOT NULL,
-  guest_id int NOT NULL,
+  reservation_id int NOT NULL,
   review varchar NOT NULL,
-  rate int NOT NULL
-  FOREIGN KEY (room_id) REFERENCES room (id),
-  FOREIGN KEY (guest_id) REFERENCES guest (id)
+  rate int NOT NULL,
+  FOREIGN KEY (reservation_id) REFERENCES reservation (id)
 );
 
 
-INSERT INTO abn_user (username, email, phone) VALUES
-                                                    ('Nick', 'nick23@mail', '12345678901'),
-                                                    ('Paola', 'paolasmith@mail', '34567892034'),
-                                                    ('Bob', 'bobbybrown@mail', '480985123545'),
-                                                    ('Max', 'max@mail', '345766885543'),
-                                                    ('Katrin', 'kate@mail', '23785490345');
+INSERT INTO abn_user (username, email, phone, user_type) VALUES
+                                                    ('Nick', 'nick23@mail', '12345678901', 'host'),
+                                                    ('Paola', 'paolasmith@mail', '34567892034', 'host'),
+                                                    ('Bob', 'bobbybrown@mail', '480985123545', 'host'),
+                                                    ('Max', 'max@mail', '345766885543', 'guest'),
+                                                    ('Katrin', 'kate@mail', '23785490345', 'guest');
 
-INSERT INTO host (abn_user_id) VALUES (1), (2), (3);
-
-INSERT INTO guest (abn_user_id) VALUES (3), (4), (5);
-
-INSERT INTO room (host_id,
+INSERT INTO room (abn_user_id,
                 price_night,
                 amount_of_residents,
                 n_of_rooms,
                 n_of_beds,
                 air_conditioning,
                 refrigerator,
-                kitchen) VALUES
+                kitchen
+                ) 
+                VALUES
                 (1, 75.5, 2, 1, 2, TRUE, TRUE, TRUE),
                 (2, 90.0, 3, 2, 2, TRUE, TRUE, TRUE),
                 (3, 80, 2, 2, 2, FALSE, TRUE, TRUE);
@@ -99,45 +77,38 @@ INSERT INTO reservation (
                 check_in,
                 check_out,
                 room_id,
-                guest_id
-                ) VALUES
-                (NOW(), '2023-10-10', '2023-10-15', 1, 1),
-                (NOW(), '2023-10-10', '2023-10-15', 2, 2),
-                (NOW(), '2023-10-16', '2023-10-25', 1, 2);
-
-INSERT INTO availability (
-    room_id,
-    checking_date,
-    available) VALUES
-    (1, '2023-10-25', FALSE),
-    (1, '2023-10-26', TRUE),
-    (1, '2023-10-27', TRUE);
+                abn_user_id
+                ) 
+                VALUES
+                (NOW(), '2023-10-10', '2023-10-15', 1, 3),
+                (NOW(), '2023-10-10', '2023-10-15', 2, 4),
+                (NOW(), '2023-10-16', '2023-10-25', 1, 4);
 
 INSERT INTO payment (
     paid_at,
-    room_id,
-    guest_id,
-    payment_method
-    ) VALUES
-    (NOW(), 1, 1, 'card'),
-    (NOW(), 2, 2, 'cash'),
-    (NOW(), 1, 2, 'cash');
+    reservation_id,
+    payment_method,
+    amount
+    ) 
+    VALUES
+    (NOW(), 1, 'card', 453.00),
+    (NOW(), 2, 'card', 540.00),
+    (NOW(), 1, 'cash', 900.00);
 
 INSERT INTO review (
     review_date,
-    room_id,
-    guest_id,
+    reservation_id,
     review,
-    rate) VALUES
-    (NOW(), 1, 1, 'great', 5),
-    (NOW(), 2, 2, 'Everything is ok', 5),
-    (NOW(), 1, 2, 'recommend this room', 5);
-
+    rate
+    ) 
+    VALUES
+    (NOW(), 1, 'great', 5),
+    (NOW(), 2, 'Everything is ok', 5),
+    (NOW(), 3, 'recommend this room', 5);
  
-SELECT u.username, u.id, COUNT(r.guest_id) AS biggest_traveler
+SELECT u.username, u.id, COUNT(r.abn_user_id) AS biggest_traveler
 FROM reservation r
-LEFT JOIN guest g ON g.id = r.guest_id
-LEFT JOIN abn_user u ON u.id = g.abn_user_id
+LEFT JOIN abn_user u ON u.id = r.abn_user_id
 GROUP BY u.username, u.id
 ORDER BY biggest_traveler DESC
 LIMIT 1;
