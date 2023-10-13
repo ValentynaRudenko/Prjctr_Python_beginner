948315f71b5254ab53c73efd9b71471fe2cc53b3
Valentyna <valentino4ka2009@gmail.com>
2023-10-13 14:37:56 +0300

    corrected

--- hw_17/task_1.py
+++ hw_17/task_1.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Float, Boolean, Date, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Date, ForeignKey, Double
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, relationship, joinedload
+from sqlalchemy.orm import sessionmaker, relationship
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -12,31 +13,31 @@ password = os.getenv('password')
 
 Base = declarative_base()
 
+
 class Users(Base):
     __tablename__ = 'abn_user'
 
     id = Column(Integer, primary_key=True)
-    username = Column(String(50))
-    email = Column(String(50))
-    phone = Column(String(20))
-    user_type = Column(String(10))
-
+    username = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    phone = Column(String(20), nullable=False)
+    user_type = Column(String(10), nullable=False)
 
     def __repr__(self):
         return (f'User {self.username} email: {self.email},'
                 f'phone: {self.phone}, type: {self.user_type}')
- 
+
 
 class Rooms(Base):
     __tablename__ = 'room'
 
     id = Column(Integer, primary_key=True)
     abn_user_id = Column(Integer, ForeignKey('abn_user.id'))
-    price_night = Column(Float)
-    amount_of_residents = Column(Integer)
-    n_of_rooms = Column(Integer)
-    n_of_beds = Column(Integer)
-    air_conditioning = Column(Boolean)
+    price_night = Column(Double, nullable=False)
+    amount_of_residents = Column(Integer, nullable=False)
+    n_of_rooms = Column(Integer, nullable=False)
+    n_of_beds = Column(Integer, nullable=False)
+    air_conditioning = Column(Boolean, nullable=False)
 
     abn_user = relationship('Users')
 
@@ -44,14 +45,13 @@ class Rooms(Base):
 class Reservations(Base):
     __tablename__ = 'reservation'
     id = Column(Integer, primary_key=True)
-    check_in = Column(Date)
-    check_out = Column(Date)
-    room_id = Column(Integer, ForeignKey('room.id'))
-    abn_user_id = Column(Integer, ForeignKey('abn_user.id'))
+    check_in = Column(Date, nullable=False)
+    check_out = Column(Date, nullable=False)
+    room_id = Column(Integer, ForeignKey('room.id'), nullable=False)
+    abn_user_id = Column(Integer, ForeignKey('abn_user.id'), nullable=False)
 
     room = relationship('Rooms')
     abn_user = relationship('Users')
-
 
 
 DATABASE_URL = 'postgresql://{user}:{password}@{host}:{port}/{database}'
